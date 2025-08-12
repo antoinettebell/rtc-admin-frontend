e8c9488d22c7f1486816a23bd4901872093f860c
Tailor <69hemin69@gmail.com>
2025-08-12 14:28:21 +0530

    added form pages

--- src/app/(main)/vendor/page.tsx
+++ src/app/(main)/vendor/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Eye, LoaderCircle } from "lucide-react";
+import { Edit, Eye, LoaderCircle } from "lucide-react";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { Column, DataTable } from "@/components/ui/data-table";
@@ -284,7 +284,19 @@ export default function Vendors() {
     {
       header: "Action",
       fieldName: "requestStatus",
-      accessor: (d) => <Eye className="text-primary" size={18} />,
+      accessor: (d) => (
+        <div className="flex gap-2 items-center mr-2">
+          <Eye className="text-primary" size={18} />
+          <Edit
+            className="text-blue-600 p-1"
+            size={22}
+            onClick={(e) => {
+              router.push(`/vendor/edit?q=${d._id}`);
+              e.stopPropagation();
+            }}
+          />
+        </div>
+      ),
     },
   ];
 
--- src/services/food-truck-api-service.ts
+++ src/services/food-truck-api-service.ts
@@ -2,8 +2,16 @@
 import { BaseAPI } from "./base-api";
 import { APIEndpoint } from "@/models/api-endpoint";
 import { IResponse } from "@/interfaces/response-interface";
+import { FoodTruck } from "@/interfaces/user-interface";
 
 class FoodTruckApiService extends BaseAPI {
+  update(id: string, data: Partial<FoodTruck>) {
+    return this.put<IResponse<boolean>>(
+      `${APIEndpoint.FOOD_TRUCK}/${id}`,
+      data,
+    );
+  }
+
   updateExtra(id: string, featured) {
     return this.put<IResponse<boolean>>(
       `${APIEndpoint.FOOD_TRUCK}/${id}/update-extra`,
